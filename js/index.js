fdc901733d34809f6351d0669f3e71464baac442
Jonathan Van Schenck <jvschenck@novadynamics.com>
2023-08-06 15:24:23 -0700

    Refactor the js index file for clarity

--- js/index.js
+++ js/index.js
@@ -7,29 +7,35 @@ const { GameManager } = require("./lib/Game.js");
 
 const env = require("./env.js");
 
-let [ socket ] = process.argv.slice(2);
-if ( !socket ) socket = "/tmp/chess";
-
 const log = Logger.console("", env.log);
 const engine = new Engine(env.engine, log);
+const api = new API(env.api);
+const gm = new GameManager(api, engine, log);
+
+/**
+ * Gracefully tear the client down
+ *
+ * @param {?string} [sig=undefined] The name of the signal that triggered this kill (if any)
+ * @return {Promise}
+ */
+async function shutdown(sig) {
+    return Promise.all([
+        engine.stop(sig),
+        gm.stop(),
+    ]).then(() => {
+        log.info("All processes stopped, exiting now.")
+        process.exit(0); // TODO : we shouldn't need this, figure out what is hanging
+    });
+}
+
 
 !async function(){
 
+    // Start the engine first (since the init call inside the
+    //  process takes a little while to execute)
     await engine.start();
 
-    const api = new API(env.api);
-    const gm = new GameManager(api, engine, log);
-
-    const kill = (sig) => {
-        return Promise.all([
-            engine.stop(sig),
-            gm.stop(),
-        ]).then(() => {
-            log.info("All processes stopped, exiting now.")
-            process.exit(0)
-        });
-    }
-
+    // Listen for signals to call shutdown (or just kill on the second signal)
     let once = false;
     for ( const sig of ["SIGINT", "SIGTERM"] ) {
         process.on(sig, () => {
@@ -39,14 +45,18 @@ const engine = new Engine(env.engine, log);
                 process.exit(1);
             }
             once = true;
-            kill(sig);
+            shutdown(sig);
         });
     }
+
+    // Use a graceful shutdown on uncaught exceptions,
+    //  otherwise the engine child process gets orphaned.
     process.on("uncaughtException", (err) => {
         console.error(err);
-        kill();
+        shutdown();
     });
 
-
+    // Start the Game manager last, since we want everything else up and running (ish)
+    //  before we fire up any games
     await gm.start();
 }()
